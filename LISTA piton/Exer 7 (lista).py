71af3e2bec4570020dda53105ee4f42a6d18afaf
hayssa17 <hayssabarrosoh@hotmail.com>
2018-09-12 12:27:10 -0300

    Falta o 7

--- LISTA piton/Exer 7 (lista).py
+++ LISTA piton/Exer 7 (lista).py
@@ -1,6 +1,23 @@
 #Liçao 7 de lista
+
 N1=int(input('Digite a quantidade de números'))
 N2=int(input('Digite a quantidade de números'))
+soma=0
+i=0
+L1=[]
+L2=[]
 
-for i in range(0,N1):
-    num1=int(input('Digite o número'))
+while i<N1:
+    num1=int(input('Digite um número:'))
+    L1.append(num1)
+    while i<N2:
+        num2=int(input('Digite os números da outra lista :'))
+        L2.append(num2)
+    
+
+        soma=N1+N2
+print(L1)
+print(L2)
+print(soma)
+
+
